5b7ffa94da0eddc72a129905647875b5a480289f
Nogren99 <nogren23@gmail.com>
2022-09-26 23:58:10 -0300

    objects array added

--- js/portScript.js
+++ js/portScript.js
@@ -1,33 +1,49 @@
-function totalAct(cant){
-    total+= parseInt(cant)
+class activo{
+    constructor(nombre,valor){
+        this.nombre=nombre
+        this.valor=valor
+    }
+    agrega(cant){
+        this.valor+=parseInt(cant)
+    }
+    quita(cant){
+        if(cant<this.valor)
+            this.valor-=parseInt(cant)
+        else 
+            this.valor=0
+    }
 }
 
 function agregarActivo(){
-    let nuevoActivo 
-    let cant 
-    let ready=false
+
+    let nuevoActivo = prompt("Ingrese un nuevo activo")
+    let cant = prompt("ingrese el valor en dolares del activo")
+    
+    while(cant<0)
+        cant = prompt("increse un valor mayor a 0")
+
+    if(activos.includes(nuevoActivo)){//si es igual devuelve TRUE
+        let i = activos.indexOf(nuevoActivo)
+        activos[i].agrega(cant) 
+    }else
+        activos.push(new activo(nuevoActivo,cant))
+}
+
+let activos = []  //Array de objetos, cada objeto es un activo que ingresa el usuario
+let ready = false
+
+while(!ready){
     let res
 
-    while(!ready){
-        nuevoActivo = prompt("Ingrese un nuevo activo")
-        cant = prompt("increse el valor del activo")
-        while(cant<0){
-            cant = prompt("increse un valor mayor a 0")
-        }
-        totalAct(cant);
-
-        res =  prompt("Desea agregar otro activo? si/no")
+    agregarActivo()
+    res =  prompt("Desea agregar otro activo? si/no")
         if(res.toUpperCase()=="NO")
             ready=true
 
-    }
 
 }
 
-let total = 0;
-agregarActivo()
-
-prompt ("Su total actual es " + total)
-
-
-
+//muestra de array de objetos
+for (const act of activos){
+    console.log(act)
+}
